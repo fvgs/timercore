5e6675e90bf8d89870f7b3107bde32378ddf63b8
Felipe Vargas <git@fvgs.ai>
2017-04-25 05:57:32 -0400

    We've entered an endless recursion of time

---
+++ src/Timer.js
@@ -0,0 +1,78 @@
+const EventEmitter = require ('events')
+
+const {hrtime} = process
+
+const MILLISECONDS_PER_SECOND = 1000
+const INTERVAL = 1
+
+class Timer extends EventEmitter {
+	constructor (seconds) {
+		super ()
+		this.secondsRemaining = seconds
+		this.residual = 0
+		this.isStarted = false
+		this.startHrtime
+		this.secondsSinceStart
+		this.currentTimeout
+	}
+
+	start () {
+		if (this.isStarted || this.isFinished ()) return
+
+		this.startHrtime = hrtime ()
+		const interval = (INTERVAL - this.residual) * MILLISECONDS_PER_SECOND
+		this.timeout (interval)
+		this.isStarted = true
+		this.secondsSinceStart = 0
+	}
+
+	isFinished () {
+		return this.secondsRemaining <= 0
+	}
+
+	timeout (interval) {
+		this.currentTimeout = setTimeout (() => {
+			this.updateResidual ()
+			this.secondsSinceStart += INTERVAL
+			const diff = this.getTimeDiff ()
+			let nextInterval = INTERVAL - diff - this.residual
+			nextInterval *= MILLISECONDS_PER_SECOND
+			this.timeout (nextInterval)
+
+			/**
+			 * Because EventEmitter.emit calls registered listeners synchronously, it
+			 * must be possible for any public API method to be callable from within a
+			 * listener without corrupting the internal Timer state.
+			 *
+			 * For that reason, the 'tick' event is emitted after the above code
+			 */
+			this.secondsRemaining -= INTERVAL
+			this.emit ('tick', this.secondsRemaining)
+			if (this.isFinished ()) {
+				this.stop ()
+				this.emit ('beep')
+			}
+		}, interval)
+	}
+
+	updateResidual () {
+		this.residual = Math.max (this.residual - INTERVAL, 0)
+	}
+
+	getTimeDiff () {
+		const [seconds, nanoseconds] = hrtime (this.startHrtime)
+		const actualSecondsSinceStart = seconds + (nanoseconds / 1e9)
+		const diff = actualSecondsSinceStart - this.secondsSinceStart
+		return diff
+	}
+
+	stop () {
+		if (!this.isStarted) return
+
+		clearTimeout (this.currentTimeout)
+		this.isStarted = false
+		this.residual += this.getTimeDiff ()
+	}
+}
+
+module.exports = Timer
